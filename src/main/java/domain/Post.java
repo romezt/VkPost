a5143c1bc266189aad66d02caf2bd4e66c6bf37e
Anton <ikkii780@mail.ru>
2021-11-02 11:46:13 +0500

    v1

---
+++ src/main/java/domain/Post.java
@@ -0,0 +1,19 @@
+package domain;
+
+public class Post {
+
+    private String avatarUrl;
+    private String publicName;
+    private String dateAndTimeOfPost;
+    private Accordion accordion;
+    private String textOfPost;
+    private AddToFavorite addToFavorite;
+    private String nameOfArticle;
+    private String linkToArticle;
+    private LikesInfo likesInfo;
+    private CommentsInfo commentsInfo;
+    private RepostsInfo repostsInfo;
+    private ViewsInfo viewsInfo;
+
+    // + getters/setters
+}
