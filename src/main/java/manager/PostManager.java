b34170a3e3c2bd0e39dbb9bdea19e7e3135e5d3e
Anton <ikkii780@mail.ru>
2021-11-02 12:29:06 +0500

    Manager

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,13 @@
+package manager;
+
+import domain.Post;
+
+public class PostManager {
+
+    private Post[] search ( int ownerId, String domain, String query, int ownersOnly, int count, int offset ) {
+        return null;
+    }
+
+    private void delete ( int ownerId, int postId) {}
+
+}
