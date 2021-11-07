7bcd6b3c4a6d107b0aa2cbf58b19481c5d429b92
Anton <ikkii780@mail.ru>
2021-11-07 17:28:40 +0500

    Post1

---
+++ src/main/java/domain/CommentsInfo.java
@@ -0,0 +1,5 @@
+package domain;
+
+public class CommentsInfo {
+    private int count;
+}
--- src/main/java/domain/Post.java
+++ src/main/java/domain/Post.java
@@ -2,38 +2,17 @@ package domain;
 
 public class Post {
 
-    private int id;
-    private int ownerId;
-    private int fromId;
-    private int createdBy;
-    private int replyOwnerId;
-    private int replyPostId;
-    private int friendOnly;
-    private String postType;
-    private PostSource postSource;
-    private Geo geo;
-    private int signerId;
-    private int canPin;
-    private int canDelete;
-    private int canEdit;
-    private int isPinned;
-    private int markedAsAds;
-    private Donut donut;
-    private int postponedId;
-
-    
     private String avatarUrl;
     private String publicName;
-    private String dateAndTimeOfPost;    // private int date;
+    private String dateAndTimeOfPost;
     private Accordion accordion;
-    private String textOfPost;           // private String text;
-    private AddToFavorite addToFavorite; // private boolean isFavorite;
+    private String textOfPost;
+    private AddToFavorite addToFavorite;
     private String nameOfArticle;
-    private String linkToArticle;        // private Copyright copyright;
-    private LikesInfo likesInfo;         // object likes
-    private CommentsInfo commentsInfo;   // object comments
-    private RepostsInfo repostsInfo;     // object reposts
-    private ViewsInfo viewsInfo;         // object views
-
+    private String linkToArticle;
+    private LikesInfo likesInfo;
+    private CommentsInfo commentsInfo;
+    private RepostsInfo repostsInfo;
+    private ViewsInfo viewsInfo;
     // + getters/setters
 }
